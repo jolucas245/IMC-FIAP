cb826c66bdd2834d349a5572b51bc416ca1db50d
Joao Ávila <jolucas245@gmail.com>
2024-03-14 07:09:58 -0300

    :hammer: refactor: edit "Abaixo do Peso" color of card

--- app/src/main/java/com/fiap/imc/CalculateIMC.kt
+++ app/src/main/java/com/fiap/imc/CalculateIMC.kt
@@ -11,8 +11,8 @@ fun calculateIMC(weight: Double, height: Double): Double{
 fun getStatusIMC(imc: Double): Pair<String, Long>{
      return when(imc){
         in 0.0 ..16.9 -> "Muito Abaixo do Peso" to 0xfff44336
-        in 17.0..18.4 -> "Abaixo do Peso" to 0xffffeb3b
-        in 18.5..24.9 -> "Peso Ideal" to 0xff4caf50
+        in 17.0..18.4 -> "Abaixo do Peso" to 0xfffbc02d
+                 in 18.5..24.9 -> "Peso Ideal" to 0xff4caf50
         in 25.0..29.9 -> "Acima do Peso" to 0xffffeb3b
         in 30.0..34.9 -> "Obesidade Grau I" to 0xfff44336
         in 35.0..40.0 -> "Obesidade Grau II" to 0xffb71c1c
